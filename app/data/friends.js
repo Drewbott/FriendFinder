70130da434b344412191bf781a68e3d6233afe93
Andrew Norris <andrewnorris@Andrews-MacBook-Air.local>
2018-09-08 13:01:08 -0700

    Initial push

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+
+var friendsArray = [
+{
+  "name":"Ahmed",
+  "photo":"https://fortunedotcom.files.wordpress.com/2018/06/riz-ahmed.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+  "name":"Johnny Danger",
+  "photo":"http://www.hwr-blog.de/wp-content/uploads/2013/09/hipsterbeard1.jpg",
+  "scores":[
+      3,
+      3,
+      3,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+
+},
+{
+  "name":"Amanda Hugginkis",
+  "photo":"https://images.unsplash.com/photo-1529932260967-af9d3bbd8138?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b3e872d900875102bc21577a8f6f1345&auto=format&fit=crop&w=400&q=60",
+  "scores":[
+      3,
+      3,
+      3,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  }
+
+
+
+]
+
+module.exports = friendsArray
